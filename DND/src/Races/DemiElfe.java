326c4f97df69b311b61184d75169f9ab4a5d8600
Jérémie Nicolas <94701868+jeremieNicolas@users.noreply.github.com>
2024-11-21 22:45:45 -0500

    demi elfe, demi orc, début gnome

---
+++ DND/src/Races/DemiElfe.java
@@ -0,0 +1,36 @@
+package Races;
+
+import java.util.ArrayList;
+
+public class DemiElfe extends Race{
+	
+	public DemiElfe()
+	{
+		super.setNom("Demi-elfe");
+		super.caractéristiques = new ArrayList<>();
+		super.setDescription("Issus de deux mondes sans vraiment appartenir à aucu, les demi-elfes combinent ce que certains disent être les meilleures qualités de leurs parents humains et elfes : la curiosité, l'inventivité et l'ambition des humains, tempérées par le sens du raffinement, l'amour de la nature et les goûts artistiques des elfes. Certains demi-elfes vivent parmi les humains, mis à l'écart à cause de leurs différences émotionnelles et physiques, condamnés à voir ceux qu'ils aiment vieillir alorsq eu le temps les effleure à peine. D'autres vivent avec les elfes et se laissent gagner par la fébrilité quand ils atteignent l'âge adulte dans les royaumes intemporels des elfes où leurs camarades continuent de vivre comme des enfants. De nombreux demi-elfes se révèlent incapables de s'intégrer pleinement dans la société d'un de leurs parents et optent pour une vie de vagabondages solitaires ou rejoignent d'autres marginaux et optent pour une vie de vagabondages solitaires ou rejoignent d'autres marginaux et exilés pour mener une vie d'aventures.");
+		
+		Caractéristique c;
+		c = new Caractéristique("Augmentation de caractéristiques","Votre valeur de charisme augmente de 2. La valeur de deux caractéristiques au choix augmentent de 1.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Âge", "Les demi-elfes grandissent au même rythme que les humains et deveinnent matures vers 20 ans. Par contre, ils vivent bien plus longtemps que les humains et il n'est pas rare qu'ils dépassent les 180 ans.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Alignement", "Les demi-elfes partagent les tendances chaotiques de leurs ancêtres elfiques. Ils apprécient leur liberté et la créativité et n'ont aucune envie de suivre un leader ou d'en devenir un eux-mêmes. Ils n'apprécient pas qu'on leur impose des règles ou qu,on attendent des choses d'eux et se montrent parfois indignes de confiance ou tout au moins imprévisibles.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Taille", "Les demi-elfes sont de taille similaire aux humains. Ils font entre 150 et 180 centimètres et sont de taille moyenne.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Vitesse", "La vitesse des demi-elfes au sol est de 9 mètres.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Vision dans le noir", "Grâce à son sang elfique, les demi-elfes ont hérité d'une vision supérieure dans l'obscurité et dans la lumière faible. Dans un rayon de 18 mètres, il peut voir dans une zone de lumière faible comme s'il s'agissait d'une lumière vive et dans une zone d'obscurité comme s'il s'agissait d'une lumière faible. Par contre, le demi-elfe ne distingue pas les couleurs dans l'obscurité, seulement des nuances de gris.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Ascendance féérique", "Le demi-elfe est avantagé sur les jets de sauvegarde contre l'effet charmé et ne peut être contraint à dormir par la magie.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Polyvalence", "Le demi-elfe gagne la maîtrise de deux compétences au choix. ");
+		caractéristiques.add(c);
+		c = new Caractéristique("Langues", "Le demi-elfe peut parler, lire et écrire le commun, l'elfique et une autre langue au choix.");
+		caractéristiques.add(c);
+	}
+
+}
+
+
---
+++ DND/src/Races/Gnome.java
@@ -0,0 +1,39 @@
+package Races;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gnome extends Race{
+
+	public Gnome()
+	{
+		ArrayList<String> nomsMasculins = new ArrayList<String>();
+		ArrayList<String> nomsFéminins = new ArrayList<String>();
+		ArrayList<String> nomsClan = new ArrayList<String>();
+		ArrayList<String> surnoms = new ArrayList<String>();
+		
+		super.setNom("Gnome");
+		super.setDescription("Un bourdonnement trahissant une activité débordante accompagne en permanence les échevaux d'habitations souterraines et les quartiers où les gnomes s'installent en communautés très soudées. D'autres sons couvrent ponctuellement ce bourdonnement perpétuel : le crissement d'une broyeuse, une petite explosion, un cri de surprise ou de triomphe et surtout, surtout, des éclats de rire. Les gnomes se réjouissent de la vie et apprécient chaque minute passée à inventer quelque chose, à explorer, chercher, créer et jouer.");
+		super.caractéristiques = new ArrayList<Caractéristique>();
+		Caractéristique c;
+		
+		nomsMasculins.addAll(List.of("Alston","Alvyn","Boddynock","Brocc","Burgell","Dimble","Eldon","Erky","Fonkin","Frug","Gerbo","Gimble","Glim","Jebeddo","Kellen","Namfoodle","Orryn","Roondar","Seebo","Sindri","Warryn","Wren","Zook"));
+		nomsFéminins.addAll(List.of("Bimpnottine","Breena","Caramip","Carline","Donella","Duvamil","Ella","Ellyjobell","Ellywick","Lilli","Loopmottine","Lorilla","Mardnab","Nissa","Nyx","Oda","Orla","Roywyn","Shamil","Tana","Waywocket","Zanna"));
+		nomsClan.addAll(List.of("Beren","Daergek","Folklor","Garrick","Nackle","Murning","Ningel","Raulnor","Scheppen","Timbers","Turen"));
+		surnoms.addAll(List.of("Brillegemme","Canardboiteux","Cendrefoyer","Clapebière","Doubleboucle","Fnipper","Furet","Ku","Manteau","Nim","Piquepâte","Pock","Qu'unechaussure"));
+		
+		c = new Caractéristique("Augmentation de caractéristiques","La valeur d'intelligence du gnome augmente de 2");
+		caractéristiques.add(c);
+		c = new Caractéristique("Âge","Les gnomes grandissent au même rythme que les humains. On estime généralement qu'ils deviennent adultes vers 40 ans. Ils peuvent vivre de 350 à presque 500 ans.");
+		caractéristiques.add(c);
+		c = new Caractéristique("Alignement","Les gnomes sont généralement attirés par le bien. Ceux qui sont attirés par la loi sont des sages, des ingénieurs, des chercheurs, des érudits, des enquêteurs ou des inventeurs. Ceux qui sont plus attirés par le chaos deviennent ménestrels, arnaqueurs, vagabonds ou bijoutiers spécialisés en bijoux fantaisie. Les gnomes ont bon coeur et même les arnaqueurs sont finalement plus taquins que mal intentionnés.");
+		caractéristiques.add(c);
+	}
+}
+/*	public DemiElfe()
+{
+super.setNom("Demi-elfe");
+super.caractéristiques = new ArrayList<>();
+super.setDescription("Issus de deux mondes sans vraiment appartenir à aucu, les demi-elfes combinent ce que certains disent être les meilleures qualités de leurs parents humains et elfes : la curiosité, l'inventivité et l'ambition des humains, tempérées par le sens du raffinement, l'amour de la nature et les goûts artistiques des elfes. Certains demi-elfes vivent parmi les humains, mis à l'écart à cause de leurs différences émotionnelles et physiques, condamnés à voir ceux qu'ils aiment vieillir alorsq eu le temps les effleure à peine. D'autres vivent avec les elfes et se laissent gagner par la fébrilité quand ils atteignent l'âge adulte dans les royaumes intemporels des elfes où leurs camarades continuent de vivre comme des enfants. De nombreux demi-elfes se révèlent incapables de s'intégrer pleinement dans la société d'un de leurs parents et optent pour une vie de vagabondages solitaires ou rejoignent d'autres marginaux et optent pour une vie de vagabondages solitaires ou rejoignent d'autres marginaux et exilés pour mener une vie d'aventures.");
+
+Caractéris*/
